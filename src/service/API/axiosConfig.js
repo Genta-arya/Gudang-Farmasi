38f001f0b78890a62bc347067c89ec3dccc3238f
M.Gentha arya pratama <74033154+Genta-arya@users.noreply.github.com>
2024-12-18 09:19:04 +0700

    Update axiosConfig.js

--- src/service/API/axiosConfig.js
+++ src/service/API/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosConfig = {
   // baseURL: 'http://localhost:3001', 
-  baseURL: 'http://192.168.0.106:3000',
+  baseURL: 'http://192.168.0.107:3000',
   
   timeout: 50000, 
   headers: {
